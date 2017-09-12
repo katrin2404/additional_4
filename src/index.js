ba4878b5eb919ada78b94fb80b75df48121570c2
katrin2404 <sachko.ekaterina@gmail.com>
2017-09-12 22:08:55 +0300

    add multiply first and second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-let bigInt = require("big-integer");
 module.exports = function multiply(first, second) {
     // your solution
-    return bigInt(first).multiply(second).toString();
-
-};
+    return (first * second).toString();
+ };
